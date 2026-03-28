5ec66e1fa85b38e04a0897c7fb15da5e7966c4d0
Tamagochi <ayubdestama@gmail.com>
2026-03-28 16:23:51 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TP_mod5/Program.cs
+++ TP_mod5/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_mod5
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T x)
+        {
+            Console.WriteLine($"Halo user {x}");
+        }
+
+        static void Main(string[] args)
+        {
+            HaloGeneric sapaan = new HaloGeneric();
+
+            // Bagian Input:
+            Console.Write("Masukkan nama panggilan praktikan: ");
+            string namaInput = Console.ReadLine(); // Mengambil input dari user
+
+            // 3. Memanggil method dengan input yang diberikan
+            sapaan.SapaUser(namaInput);
+
+            // Menahan console agar tidak langsung tertutup
+            Console.WriteLine("\nTekan Enter untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}

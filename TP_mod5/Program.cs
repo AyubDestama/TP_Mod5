4abca040dd5978da8664d9a5419d75f108287430
Tamagochi <ayubdestama@gmail.com>
2026-03-28 16:39:08 +0700

    menambahkan class DataGeneric

--- TP_mod5/Program.cs
+++ TP_mod5/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_mod5
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T x)
+        {
+            Console.WriteLine($"Halo user {x}");
+        }
+        public class DataGeneric<T>
+        {
+ 
+            private T data;
+
+            public DataGeneric(T data)
+            {
+                this.data = data;
+            }
+
+            public void PrintData()
+            {
+                Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            HaloGeneric sapaan = new HaloGeneric();
+            Console.Write("Masukkan nama panggilan praktikan: ");
+            string namaInput = Console.ReadLine(); 
+            sapaan.SapaUser(namaInput);
+
+            Console.Write("Masukkan NIM praktikan: ");
+            string nimInput = Console.ReadLine();
+            DataGeneric<string> dataNim = new DataGeneric<string>(nimInput);
+            dataNim.PrintData();
+
+            Console.WriteLine("\nTekan Enter untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}
